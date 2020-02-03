9ff0e5cc91e8aca3a398cc831682bb33775a3820
Jaeman Cho <57745706+JaemanCho@users.noreply.github.com>
2020-02-03 16:20:23 +0900

    Set environment

--- src/main/java/com/cho/app/Application.java
+++ src/main/java/com/cho/app/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMvcBoardApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMvcBoardApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
