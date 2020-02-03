25a30ab0ff9b23c7af075ad2f14874315a3d107d
Jaeman Cho <57745706+JaemanCho@users.noreply.github.com>
2020-02-03 16:12:20 +0900

    first commit

---
+++ src/test/java/com/cho/app/SpringBootMvcBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cho.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
